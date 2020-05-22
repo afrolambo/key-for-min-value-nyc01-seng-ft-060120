31fab4cfe64371024b0160f1ece0448d03181417
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-22 18:01:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@
 def key_for_min_value(name_hash)
   min_value = 100 
  name_hash.each do |name, num|
-   if "#{name}" < min_value 
-   min_value = :name
- end 
+   if "#{num}".to_i < min_value 
+   min_value = name_hash{:name => num}
+ end
  min_value
- end 
-end
+ end
+end 
