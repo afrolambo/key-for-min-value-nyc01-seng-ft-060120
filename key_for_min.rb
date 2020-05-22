5e6fa3c9fcca1ae8f2506209c98a202e49c5a8b0
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-22 17:58:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+ name_hash.each do |name, num|
+   if :name < min_value 
+   min_value = :name
+ end 
+ min_value
 end
